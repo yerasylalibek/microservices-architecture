98e4f90837a602646e30a16a8ac3e845faaf88a4
yerasylalibek <erasyl.iitu@gmail.com>
2021-09-25 15:45:18 +0600

    add db-struct dependencies

---
+++ db-struct/src/main/java/kz/app/cart/shopping/DbStructApplication.java
@@ -0,0 +1,13 @@
+package kz.app.cart.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbStructApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbStructApplication.class, args);
+    }
+
+}
