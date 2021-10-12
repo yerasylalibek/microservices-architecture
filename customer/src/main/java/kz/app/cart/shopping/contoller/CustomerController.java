59d6e5ee25f0bd45942248709305edce5e6a16a1
SultanAssel <60469894+SultanAssel@users.noreply.github.com>
2021-10-12 08:23:05 +0600

    Update CustomerController.java

--- customer/src/main/java/kz/app/cart/shopping/contoller/CustomerController.java
+++ customer/src/main/java/kz/app/cart/shopping/contoller/CustomerController.java
@@ -1,47 +1,45 @@
-package kz.app.cart.shopping.contoller;
+ package kz.app.cart.shopping.contoller;
+ import kz.app.cart.shopping.dto.CustomerDTO;
+ import kz.app.cart.shopping.model.Customer;
+ import kz.app.cart.shopping.service.ICustomerService;
+ import lombok.AllArgsConstructor;
+ import lombok.extern.slf4j.Slf4j;
+ import org.springframework.http.ResponseEntity;
+ import org.springframework.web.bind.annotation.*;
+ import java.util.List;
 
-import kz.app.cart.shopping.dto.CustomerDTO;
-import kz.app.cart.shopping.model.Customer;
-import kz.app.cart.shopping.service.ICustomerService;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+ @RestController
+ @Slf4j
+ @AllArgsConstructor
+ public class CustomerController {
 
-import java.util.List;
+     private final ICustomerService customerService;
+ 
+     @PostMapping
+     public ResponseEntity<Customer> save(@RequestBody CustomerDTO customerDTO) {
+         log.info("customerDTO : " + customerDTO.toString());
+         return ResponseEntity.ok(customerService.save(customerDTO));
+     }
+ 
+     @RequestMapping(method = RequestMethod.POST, value = "/test")
+     public ResponseEntity<?> update(@RequestBody CustomerDTO customerDTO) {
+         log.info("here");
+         return ResponseEntity.ok(customerService.save(customerDTO));
+     }
+ 
+     @GetMapping("/{id}")
+     public ResponseEntity<Customer> getById(@PathVariable Long id){
+         return ResponseEntity.ok(customerService.getById(id));
+     }
+  
+     @DeleteMapping("/{id}")
+     public void deleteById(@PathVariable Long id){
+         customerService.deleteById(id);
+     } 
 
-@RestController
-@Slf4j
-@AllArgsConstructor
-public class CustomerController {
+     @GetMapping
+     public ResponseEntity<List<Customer>> getAll() {
+         return ResponseEntity.ok(customerService.getAll());
+     }
 
-    private final ICustomerService customerService;
-
-    @PostMapping
-    public ResponseEntity<Customer> save(@RequestBody CustomerDTO customerDTO) {
-        log.info("customerDTO : " + customerDTO.toString());
-        return ResponseEntity.ok(customerService.save(customerDTO));
-    }
-
-    @RequestMapping(method = RequestMethod.POST, value = "/test")
-    public ResponseEntity<?> update(@RequestBody CustomerDTO customerDTO) {
-        log.info("here");
-        return ResponseEntity.ok(customerService.save(customerDTO));
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Customer> getById(@PathVariable Long id){
-        return ResponseEntity.ok(customerService.getById(id));
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable Long id){
-        customerService.deleteById(id);
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Customer>> getAll() {
-        return ResponseEntity.ok(customerService.getAll());
-    }
-
-}
+ }
