b3e65685dbed2fd29ceedd0c6bceed61ed9a9988
SultanAssel <60469894+SultanAssel@users.noreply.github.com>
2021-10-12 08:59:06 +0600

    Update CustomerDTO.java

--- customer/src/main/java/kz/app/cart/shopping/dto/CustomerDTO.java
+++ customer/src/main/java/kz/app/cart/shopping/dto/CustomerDTO.java
@@ -11,27 +11,39 @@ import lombok.Data;
 public class CustomerDTO {
 
     @JsonProperty("id")
-    @ApiModelProperty(example = "1", name = "Id")
+    @ApiModelProperty(example = "1", name = "Customer id")
     private Long id;
 
     @JsonProperty("customerCode")
-    @ApiModelProperty(example = "1", name = "Code")
+    @ApiModelProperty(example = "1", name = "Customer code")
     private String customerCode;
 
+    @JsonProperty("customerName")
+    @ApiModelProperty(required = true, example = "Assel", name = "Customer name")
+    private String customerName;
+
     @JsonProperty("avatar")
-    @ApiModelProperty(example = "Assel", name = "Name")
+    @ApiModelProperty(example = "Assel", name = "Customer name")
     private byte[] avatar;
 
     @JsonProperty("emailAddress")
-    @ApiModelProperty(required = true, example = "aselek.m.s@gmail.com", name = "Email")
+    @ApiModelProperty(required = true, example = "aselek.m.s@gmail.com", name = "Customer email")
     private String emailAddress;
 
+    @JsonProperty("contactNumber")
+    @ApiModelProperty(example = "+77072001521", name = "Customer contact number")
+    private String contactNumber;
+
+    @JsonProperty("completeAddress")
+    @ApiModelProperty(example = "Almaty, Manas 66", name = "Customer address")
+    private String completeAddress;
+
     @JsonProperty("username")
-    @ApiModelProperty(required = true, example = "AsselSultan", name = "Username")
+    @ApiModelProperty(required = true, example = "AsselSultan", name = "Customer username")
     private String username;
 
     @JsonProperty("password")
-    @ApiModelProperty(required = true, example = "hello", name = "Password")
+    @ApiModelProperty(required = true, example = "hello", name = "Customer password")
     private String password;
 
 }
